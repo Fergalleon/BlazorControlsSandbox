c2a916ae30d004ef54060484eeb961435f693039
Paul Anderson <paul.anderson@cognitivegeology.com>
2021-05-28 14:01:50 +0100

    Removed Js dependencies.

--- BlazorControlsSandbox/Components/FlowChart.razor.cs
+++ BlazorControlsSandbox/Components/FlowChart.razor.cs
@@ -11,18 +11,27 @@
 
     public partial class FlowChart
     {
-        [Inject]
-        public ISvgItemService SvgItemService { get; set; }
-
-        [Inject]
-        public IJSRuntime JsRuntime { get; set; }
-
         private bool _isMouseDown = false;
 
+        [Inject] public ISvgItemService SvgItemService { get; set; }
+
+        [Inject] public IJSRuntime JsRuntime { get; set; }
+
+        private Point ContainerOffset { get; set; }
+
+        private SvgAttributes SelectedItem { get; set; }
+
+        private Point ClickPoint { get; set; }
+
+        private string SelectedItemText => SelectedItem?.DisplayValue ?? "(none)";
+
+        private List<SvgAttributes> SvgComponents { get; set; }
+        
         private void HandleMouseDown(MouseEventArgs e)
         {
-            ClickPoint = new Point((int)e.ClientX, (int)e.ClientY);
-            SelectedItem = SvgComponents?.GetSelected(ContainerOffset, ClickPoint);
+            ClickPoint = new Point((int) e.OffsetX, (int) e.OffsetY);
+
+            SelectedItem = SvgComponents?.GetSelected(ClickPoint);
             _isMouseDown = true;
         }
 
@@ -35,8 +44,8 @@
             }
 
             // todo: (hack) this needs to take into account click pos relative to item being dragged's top left, and offset.
-            SelectedItem.X = (int)e.OffsetX;
-            SelectedItem.Y = (int)e.OffsetY;
+            SelectedItem.X = (int) e.OffsetX;
+            SelectedItem.Y = (int) e.OffsetY;
         }
 
         private void HandleMouseUp(MouseEventArgs e)
@@ -44,37 +53,9 @@
             _isMouseDown = false;
         }
 
-        private SvgAttributes SelectedItem { get; set; }
-
-        private Point ClickPoint { get; set; }
-
-        private string SelectedItemText => SelectedItem?.DisplayValue ?? "(none)";
-
-        private List<SvgAttributes> SvgComponents { get; set; }
-
         protected override async Task OnInitializedAsync()
         {
             SvgComponents = await SvgItemService.GetSvgItems();
         }
-
-        protected override async Task OnAfterRenderAsync(bool firstRender)
-        {
-            if (firstRender)
-            {
-                await SetCanvasPosition();
-            }
-
-            await base.OnAfterRenderAsync(firstRender);
-        }
-
-        private async Task SetCanvasPosition()
-        {
-            var result = await JsRuntime.InvokeAsync<BoundingClientRect>(
-                "MyDOMGetBoundingClientRect", "canvas");
-
-            ContainerOffset = new Point((int)result.Left, (int)result.Top);
-        }
-
-        private Point ContainerOffset { get; set; }
     }
 }
--- BlazorControlsSandbox/Components/SvgAttributeExtensions.cs
+++ BlazorControlsSandbox/Components/SvgAttributeExtensions.cs
@@ -7,26 +7,26 @@
 
     public static class SvgAttributeExtensions
     {
-        public static SvgAttributes GetSelected(this List<SvgAttributes> svgAttributes, Point containerOffset, Point clickPoint)
+        public static SvgAttributes GetSelected(this List<SvgAttributes> svgAttributes, Point clickPoint)
         {
-            return svgAttributes?.FirstOrDefault(svg => svg.IsWithinBoundary(containerOffset, clickPoint));
+            return svgAttributes?.FirstOrDefault(svg => svg.IsWithinBoundary(clickPoint));
         }
-        private static bool IsWithinBoundary(this SvgAttributes svgAttributes, Point containerOffset, Point clickPoint)
+        private static bool IsWithinBoundary(this SvgAttributes svgAttributes, Point clickPoint)
         {
-           return IsWithinWidthBoundary(svgAttributes, containerOffset.X, clickPoint.X) && 
-                  IsWithinHeightBoundary(svgAttributes, containerOffset.Y,  clickPoint.Y);
-        }
-        
-        private static bool IsWithinWidthBoundary(SvgAttributes svgAttributes, int offsetX, int x)
-        {
-            return x >= svgAttributes.X &&
-                   x <= svgAttributes.X + svgAttributes.Width + offsetX;
+            return IsWithinWidthBoundary(svgAttributes, clickPoint.X) &&
+                   IsWithinHeightBoundary(svgAttributes, clickPoint.Y);
         }
 
-        private static bool IsWithinHeightBoundary(SvgAttributes svgAttributes, int offsetY, int y)
+        private static bool IsWithinWidthBoundary(SvgAttributes svgAttributes, int x)
+        {
+            return x >= svgAttributes.X &&
+                   x <= svgAttributes.X + svgAttributes.Width;
+        }
+
+        private static bool IsWithinHeightBoundary(SvgAttributes svgAttributes, int y)
         {
             return y >= svgAttributes.Y &&
-                   y <= svgAttributes.Y + svgAttributes.Height + offsetY;
+                   y <= svgAttributes.Y + svgAttributes.Height;
         }
     }
 }
